dba8ff978b0c5b3feabce01c59dcf5cd6cbf6b12
JohnOConnor110102 <johnoc1712@gmail.com>
2023-03-24 12:10:53 -0300

    `*pokemon_copiar` compila

--- src/pokemon.c
+++ src/pokemon.c
@@ -3,11 +3,15 @@
 #include <stdio.h>
 #include "pokemon_privado.h"
 
-#define FORMATO_LECTURA_STRING "%lud,%[^,],%lud,%s"
+#define FORMATO_LECTURA_STRING "%lu,%[^,],%lu,%s"
 const int CANT_CAMPOS_POKEMON_T = 4;
+const int STRING_VACIO = 0;
 
 pokemon_t *pokemon_crear_desde_string(const char *string)
 {
+	if (string == NULL || strlen(string) == STRING_VACIO)
+		return NULL;
+
 	pokemon_t *poke_leido = malloc(sizeof(pokemon_t));
 	if (poke_leido == NULL)
 		return NULL;
@@ -24,11 +28,23 @@ pokemon_t *pokemon_crear_desde_string(const char *string)
 
 pokemon_t *pokemon_copiar(pokemon_t *poke)
 {
-	return NULL;
+	pokemon_t *nuevo_poke = malloc(sizeof(pokemon_t));
+	if (nuevo_poke == NULL)
+		return NULL;
+
+	nuevo_poke->id = poke->id;
+	nuevo_poke->salud = poke->salud;
+	strcpy(nuevo_poke->nombre, poke->nombre);
+	strcpy(nuevo_poke->nombre_entrenador, poke->nombre_entrenador);
+
+	return nuevo_poke;
 }
 
 bool pokemon_son_iguales(pokemon_t *pokemon1, pokemon_t *pokemon2)
 {
+	if () {
+	}
+
 	return true;
 }
 
@@ -54,4 +70,5 @@ size_t pokemon_id(pokemon_t *pokemon)
 
 void pokemon_destruir(pokemon_t *pkm)
 {
+	free(pkm);
 }
